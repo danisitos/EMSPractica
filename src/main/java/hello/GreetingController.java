cfc2ab996b20a552ac54ee54c13a4ca8c7959130
daniel.jalvarez <daniel.jalvarez@alumnos.upm.es>
2024-03-05 10:57:07 +0100

    create home page issue #1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,4 +1,4 @@
-package main.java.hello;
+package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
